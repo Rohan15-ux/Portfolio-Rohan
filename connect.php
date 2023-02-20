83dfe6d2214f888da749a2a0e9fb56b28190d3b4
Rohan Chetry <73774249+Rohan15-ux@users.noreply.github.com>
2023-02-20 20:26:06 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -10,7 +10,7 @@
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
 		$stmt = $conn->prepare("insert into req(name, email, message) values(?,?,?)");
-		$stmt->bind_param("sssssi", $name,$email,$message);
+		$stmt->bind_param("sss", $name,$email,$message);
 		$execval = $stmt->execute();
 		echo $execval;
 		echo "Registration successfully...";
